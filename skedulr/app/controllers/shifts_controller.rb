fa1335e20343479d342d53a6bc564f7a0bd59c94
Rupert Saxton <rupertsaxton@gmail.com>
2017-05-02 17:56:03 -0400

    Show current shifts, form to create new shifts, cannot save new shifts

--- skedulr/app/controllers/shifts_controller.rb
+++ skedulr/app/controllers/shifts_controller.rb
@@ -1,8 +1,26 @@
 class ShiftsController < ApplicationController
+  include ShiftsHelper
 
   before_action :authenticate_employee!
 
   def index
     @first_name = current_employee.first_name
+    @employee = current_employee
   end
+
+  def create
+    @shift = Shift.new(shift_params)
+    @shift.employee_id = params[:employee_id]
+    @shift.save
+
+    redirect_to shifts_path
+  end
+
+  def new
+    @shift= Shift.new
+  end
+
+  def edit
+  end
+
 end
