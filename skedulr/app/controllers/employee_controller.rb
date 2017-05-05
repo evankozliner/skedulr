ba2b8498b8ab6e84d07edfafb1bc31536799d7d6
Rupert Saxton <rupertsaxton@gmail.com>
2017-05-05 08:50:54 -0400

    Adding rerouting for employee profile

--- skedulr/app/controllers/employee_controller.rb
+++ skedulr/app/controllers/employee_controller.rb
@@ -1,2 +1,11 @@
 class EmployeeController < ApplicationController
+
+  def create
+    @employee = current_employee
+    @employee.update(employee_params)
+  end
+
+  def new
+  end
+
 end
--- skedulr/app/helpers/employee_helper.rb
+++ skedulr/app/helpers/employee_helper.rb
@@ -1,2 +1,5 @@
 module EmployeeHelper
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :is_manager, :encrypted_password, :manager_table_id)
+  end
 end
