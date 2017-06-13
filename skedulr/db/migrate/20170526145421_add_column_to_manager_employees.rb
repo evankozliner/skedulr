458806adf7c6b50041b725f711c08cb13c26112a
Evan Kozliner <evankozliner@gmail.com>
2017-06-13 15:59:26 -0400

    Remove column removal done locally

--- skedulr/db/migrate/20170526145421_add_column_to_manager_employees.rb
+++ skedulr/db/migrate/20170526145421_add_column_to_manager_employees.rb
@@ -1,7 +1,5 @@
 class AddColumnToManagerEmployees < ActiveRecord::Migration[5.0]
   def change
-    remove_column :manager_employees, :manager_id, :integer
-    remove_column :manager_employees, :employee_id, :integer
     add_column :manager_employees, :manager_id, :integer
     add_column :manager_employees, :employee_id, :integer
     add_column :manager_employees, :business_id, :integer
