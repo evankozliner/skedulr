97344e394deb4990b52796e801b3a2ab6af6f078
Evan Kozliner <evankozliner@gmail.com>
2017-06-13 16:10:21 -0400

    Missing table creation for manager employees

--- skedulr/db/migrate/20170526145421_add_column_to_manager_employees.rb
+++ skedulr/db/migrate/20170526145421_add_column_to_manager_employees.rb
@@ -1,7 +1,9 @@
 class AddColumnToManagerEmployees < ActiveRecord::Migration[5.0]
   def change
-    add_column :manager_employees, :manager_id, :integer
-    add_column :manager_employees, :employee_id, :integer
-    add_column :manager_employees, :business_id, :integer
+    create_table :manager_employees do |t|
+      t.integer :manager_id
+      t.integer :employee_id
+      t.integer :business_id
+    end
   end
 end
